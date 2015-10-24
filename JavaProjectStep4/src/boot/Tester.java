a9e87ecc6d8b07d4749ce580a6b2907923f5692d
libermanguy <guyliberman@gmail.com>
2015-10-24 20:42:27 +0300

    Properties eli ha maniya

--- JavaProjectStep4/src/boot/Tester.java
+++ JavaProjectStep4/src/boot/Tester.java
@@ -1,59 +1,15 @@
 package boot;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.concurrent.Callable;
-
-import algorithms.mazeGenerators.Maze3d;
-import algorithms.mazeGenerators.MyMaze3dGenerator;
-import general.Position;
-import general.Solution;
-import io.MyCompressorOutputStream;
-import io.MyDecompressorInputStream;
-import model.SearchableMaze;
+import general.Properties;
 
 public class Tester {
 
 	public static void main(String[] args) throws Exception {
-		Maze3d maze = new MyMaze3dGenerator().generate(3, 3, 3);
-		Maze3d maze2 = maze;
-		HashMap<Maze3d, Solution<Position>> map = new HashMap<Maze3d, Solution<Position>>();
-		map.put(maze, null);
-		System.out.println(map.containsKey(maze2));
-		System.out.println(maze.get_maze3d().hashCode());
-		System.out.println(maze2.get_maze3d().hashCode());
-		
-		OutputStream out=new MyCompressorOutputStream(new FileOutputStream("c:\\temp\\new.maz"));
-		out.write(maze.toByteArray());
-		out.flush();
-		out.close();	
-	
-		
-		
-		InputStream in;
-		in = new MyDecompressorInputStream(new FileInputStream("c:\\temp\\new.maz"));
-		ArrayList<Integer> buff = new ArrayList<Integer>();
-		int current = in.read();
-		while (current != -1)
-		{
-		 buff.add(current);
-		 current = in.read();
-		}
-		in.close();
-		byte fileData[]=new byte[buff.size()];
-		for ( int i = 0 ; i < buff.size() ; i ++ )
-			fileData[i] = buff.get(i).byteValue();
-		maze2 = new Maze3d(fileData);
-		System.out.println(maze2.get_maze3d().hashCode());
-		System.out.println(map.containsKey(maze2));
-		maze.print();
-		maze2.print();
-
+			Properties prop = new Properties();
+			
+			prop.saveProp("c:\\temp\\test23.xml");
+			
+			prop.loadProp("c:\\temp\\test23.xml");
 	}
 
 }
--- JavaProjectStep4/src/general/Properties.java
+++ JavaProjectStep4/src/general/Properties.java
@@ -1,8 +1,78 @@
 package general;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 
-@SuppressWarnings("serial")
-public class Properties implements Serializable {
+import org.w3c.dom.Element;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
+import org.eclipse.swt.widgets.List;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+@SuppressWarnings("serial")
+public class Properties implements Serializable 
+{
+	int threadcount;
+	String workspace;
+	int display;
+	
+	public Properties() {
+		threadcount =0;
+		display=0;
+		workspace="omri ya homo";
+	}
+	
+	public void loadProp(String file) throws Exception
+	{
+		File inputfile = new File(file);
+		DocumentBuilderFactory dbfactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dbuilder = dbfactory.newDocumentBuilder();
+		Document doc = dbuilder.parse(inputfile);
+		doc.getDocumentElement().normalize();
+		NodeList nlist = doc.getElementsByTagName("properties");
+		Node n = nlist.item(0);
+		Element e = (Element)n;
+		System.out.println(e.getElementsByTagName("threadcount").item(0).getTextContent());
+		System.out.println(e.getElementsByTagName("workspace").item(0).getTextContent());
+		System.out.println(e.getElementsByTagName("display").item(0).getTextContent());
+	}
+	
+	public void saveProp(String file) throws Exception
+	{
+		File outputfile = new File(file);
+		DocumentBuilderFactory dbfactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dbuilder = dbfactory.newDocumentBuilder();
+		Document doc = dbuilder.newDocument();
+		Element e = doc.createElement("properties");
+		doc.appendChild(e);
+		
+		Element e2 = doc.createElement("threadcount");
+		e2.setTextContent(threadcount+"");
+		e.appendChild(e2);
+		
+		
+		Element e3 = doc.createElement("workspace");
+		e3.setTextContent(workspace);
+		e.appendChild(e3);
+		
+		Element e4 = doc.createElement("display");
+		e4.setTextContent(display+"");
+		e.appendChild(e4);
+		
+		
+		TransformerFactory ts = TransformerFactory.newInstance();
+		Transformer transformer = ts.newTransformer();
+		DOMSource dm = new DOMSource(doc);
+		transformer.transform(dm, new StreamResult(outputfile));
+		}
 }
